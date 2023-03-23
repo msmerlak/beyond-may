32db0d383d4350a11419f0b9222e0b95ca600e1e
Matteo Smerlak <matteo.smerlak@gmail.com>
2023-03-23 21:23:22 +0100

    let's time alpha-beta

--- scripts/alpha-beta.jl
+++ scripts/alpha-beta.jl
@@ -1,121 +1,31 @@
-using DrWatson, Glob, Revise
+using DrWatson
 @quickactivate
+
+using Glob, Revise
 foreach(includet, glob("*.jl", srcdir()))
 
-using ProgressMeter, ThreadsX
 using Plots, LaTeXStrings
 gr(label = false, grid = false)
-using Contour
 
-function plot_contour_line!(plt, ϕ, P; level = 0.5, label)
-    c = Contour.contours(
-    P[:α], 
-    P[:β],
-    Matrix(reshape(ϕ, length(P[:α]), length(P[:β]))'),
-    [level]
-    )
-    for cl in levels(c)
-        # for line in lines(cl)
-            plot!(plt, coordinates(lines(cl)[1])..., lw = 2, ls = :dash, label = label)
-        # end
+function σ_critical(α, β = 1)
+    p = Dict{Symbol, Any}(
+        :scaled => true,
+        :S => 50,
+        :μ => .5,
+        :α => α,
+        :β => β,
+        :γ => 1.,
+        :extinction_threshold => 1e-4,
+        :dist => "gamma",
+        :N => 1,
+    );
+
+    for σ in 0.01:.1:5.
+        p[:σ] = σ
+        s = stability!(p)
+        s < .9 && return σ
     end
-    return current()
 end
-
-P = Dict{Symbol, Any}(
-        :scaled => false,
-        :S => 100,
-        :μ => 1e-2,
-        :μₛ => true,
-        :σ => 1e-4,
-        :α => 0.25:.25:2.5,
-        :β => 0.25:.25:2.5,
-        :γ => 1.,
-        :threshold => false,
-        :dist => "normal",
-        :N => 1,
-    );
-
-plt = plot(x->x, xlims = (0, 2.5), color = :black, lw = 2)
-xlabel!(L"\alpha")
-ylabel!(L"\beta")
-for σ ∈ (1e-3, 5e-3, 1e-2) 
-    P[:σ] = σ
-    ϕ = ThreadsX.collect(diversity!(p) for p in expand(P));
-    plot_contour_line!(plt, ϕ, P; label = "σ = $(P[:σ])")
-end
-current()
-#savefig(plotsdir("alpha-beta"))
-
-#= alternative  =#
-P = Dict{Symbol, Any}(
-        :scaled => true,
-        :S => 50,
-        :μ => 1.,
-        :μₛ => true,
-        :σ => .1,
-        :α => 1.,
-        :β => 1.25,
-        :γ => 1.,
-        :threshold => false,
-        :dist => "normal",
-        :N => 1,
-    );
-
-plt = plot(x->x, xlims = (0, 2), color = :black, lw = 4,     label = latexstring("σ_c = 0"))
-xlabel!(L"\alpha")
-ylabel!(L"\beta")
-for β ∈ 1.5:.5:2.5 
-    P[:β] = β
-    σₘ = round(σ_crit_gauss(P), digits = 1)
-    plot!(x-> β-1+x, 
-    xlims = (0, 2), 
-    ylims = (0, 2), 
-    lw = 4,
-#    fillrange = x -> β-1.25+x,
-#    fillalpha = .5,
-    palette = cgrad(:Reds_4, rev=false),
-    label = latexstring("σ_c = $(σₘ)"), 
-    legend = :bottomright,
-    )
-end
-current()
-
-#= σ critical =#
-p = Dict{Symbol, Any}(
-        :scaled => true,
-        :S => 50,
-        :μ => 1.,
-        :μₛ => true,
-        :σ => .1,
-        :α => 1.,
-        :β => 1.25,
-        :γ => 1.,
-        :threshold => false,
-        :dist => "normal",
-        :N => 1,
-    );
-
-B = 1.5:.5:3.
-σₘ = ones(length(B))
-for (i, β) ∈ enumerate(B)
-    p[:β] = β
-    σₘ[i] = σ_crit_gauss(p)
-end
-
-σₘ
-
-plot(B, σₘ, 
-lw = 4,
-lc = :black)
-
-plt = plot(x->x, xlims = (0, 1), color = :black, lw = 2)
-xlabel!(L"\alpha")
-ylabel!(L"\beta")
-for σ ∈ (0.01, .3, .5) 
-    P[:σ] = σ
-    ϕ = ThreadsX.collect(stability!(p) for p in expand(P));
-    plot_contour_line!(plt, ϕ, P; label = "σ = $(P[:σ])")
-end
-current()
-savefig(plotsdir("alpha-beta"))
+ 
+α_range = .1:.1:1.
+@time map(σ_critical, α_range)
--- scripts/single-run.jl
+++ scripts/single-run.jl
@@ -4,22 +4,22 @@ foreach(includet, glob("*.jl", srcdir()))
 
 using Plots, LaTeXStrings
 
-p = Dict{Symbol, Any}(
+p = Dict{Symbol,Any}(
     :scaled => true,
     :S => 100,
     :μ => 0.5,
-    :σ => .05,
-    :α => .5,
-    :β => 1.,
-    :γ => 1.,
+    :σ => 0.05,
+    :α => 0.5,
+    :β => 1.0,
+    :γ => 1.0,
     :extinction_threshold => 1e-4,
     :dist => "gamma",
     :N => 1,
     :seed => rand(UInt)
 );
 
-evolve!(p; trajectory = true);
-plot(p[:trajectory], label = false)
+evolve!(p; trajectory=true);
+plot(p[:trajectory], label=false)
 p[:converged]
 p[:equilibrium]
 
@@ -29,45 +29,45 @@ p[:converged]
 #= trajectories =#
 plot(
     p[:trajectory],
-    ylabel = L"x_i(t)",
-    xlabel = L"t",
-    yscale = :log,
-    linewidth = 1,
-    legend = false,
-    grid = false,
+    ylabel=L"x_i(t)",
+    xlabel=L"t",
+    yscale=:log,
+    linewidth=1,
+    legend=false,
+    grid=false,
 )
 
 #= density distribution =#
 p_eq = p[:trajectory].u[end]
 histogram(p_eq,
-    normalize = true,
-    alpha = .5,
-    color = :green,
-    ylabel = L"P(x_*)",
-    xlabel = L"x_*",
-    label = false,
-    grid = false,
+    normalize=true,
+    alpha=0.5,
+    color=:green,
+    ylabel=L"P(x_*)",
+    xlabel=L"x_*",
+    label=false,
+    grid=false,
 )
 
 #= cavity solution with gaussian approximation =#
-X=[x for x in .9*minimum(p_eq):.0001:1.1*maximum(p_eq)]
-plot!(X,[pdf(P_gauss(p), x) for x in X],
-labels = false, #"cavity - gaussian",
-linewidth = 2,
-alpha = 1,
-linecolor = :black,
-linestyle = :dash,
-grid = false
+X = [x for x in 0.9*minimum(p_eq):0.0001:1.1*maximum(p_eq)]
+plot!(X, [pdf(P_gauss(p), x) for x in X],
+    labels=false, #"cavity - gaussian",
+    linewidth=2,
+    alpha=1,
+    linecolor=:black,
+    linestyle=:dash,
+    grid=false
 )
 
 a, b, c = Cavity(p)
 
-X=[x for x in .9*minimum(p_eq):.01:1.1*maximum(p_eq)]
-plot!(X,[P_n(x, b, c, p) for x in X],
-labels = false, #"cavity - gaussian",
-linewidth = 2,
-alpha = 1,
-linecolor = :black,
-grid = false,
-size = (600,200)
+X = [x for x in 0.9*minimum(p_eq):0.01:1.1*maximum(p_eq)]
+plot!(X, [P_n(x, b, c, p) for x in X],
+    labels=false, #"cavity - gaussian",
+    linewidth=2,
+    alpha=1,
+    linecolor=:black,
+    grid=false,
+    size=(600, 200)
 )
