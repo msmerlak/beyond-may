3cf62e5b5ed6292b2c28e7be400c58ee9c4e385a
Matteo Smerlak <matteo.smerlak@gmail.com>
2023-03-23 13:23:51 +0100

    metrics

--- src/metrics.jl
+++ src/metrics.jl
@@ -1,4 +1,3 @@
-
 function diversity!(p)
     # run N simulates and append results to p
     diversity = Vector{Float64}(undef, p[:N])
@@ -8,28 +7,25 @@ function diversity!(p)
         add_interactions!(p)
         add_initial_condition!(p)
         evolve!(p)
-       
-        diversity[i] = p[:diversity]
         
-    #=     q = p[:equilibrium]/sum(p[:equilibrium])
-        diversity[i] = 1/(sum(q.^2) * p[:S]) =#
+        q = p[:equilibrium]/sum(p[:equilibrium])
+        diversity[i] = 1/(sum(q.^2) * p[:S])
     end
 
     return mean(diversity)
 end
 
-function full_coexistence!(p)
+function stability!(p)
     # run N simulates and append results to p
-    full_coexistence = Vector{Float64}(undef, p[:N])
+    stability = Vector{Float64}(undef, p[:N])
     p[:rng] = MersenneTwister()
 
     for i in 1:p[:N]
         add_interactions!(p)
         add_initial_condition!(p)
         evolve!(p)
-        # full_coexistence[i] = p[:converged] && p[:richness] == p[:S] ? 1 : 0
-        full_coexistence[i] = p[:richness] == p[:S] ? 1 : 0
+        stability[i] = p[:converged] && p[:richness] == p[:S] ? 1 : 0
     end
 
-    return mean(full_coexistence)
+    return mean(stability)
 end
