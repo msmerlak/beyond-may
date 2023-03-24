eddc1af4da5844fa82ca6aab67da38ede2379b8d
Matteo Smerlak <matteo.smerlak@gmail.com>
2023-03-24 14:02:50 +0100

    critical sigma

--- scripts/alpha-beta.jl
+++ scripts/alpha-beta.jl
@@ -10,24 +10,25 @@ gr(label = false, grid = false)
 function σ_critical(α, β = 1)
     p = Dict{Symbol, Any}(
         :scaled => true,
-        :S => 50,
+        :S => 100,
         :μ => .5,
         :α => α,
         :β => β,
         :γ => 1.,
         :extinction_threshold => 1e-4,
         :dist => "gamma",
-        :N => 1,
+        :N => 4,
     );
 
-    for σ in 0.01:.1:5.
+    for σ in 0.0001:.01:5
         p[:σ] = σ
         s = stability!(p)
         s < .9 && return σ
     end
 end
 
-σ_critical(.3)
+Σ = [β > α ? σ_critical(α, β) : 0 for α in .1:.1:1., β in .1:.1:1.]
 
-α_range = .1:.1:1.
-@time map(σ_critical, α_range)
+Σ = [σ_critical(α, β) for α in .1:.1:1., β in .1:.1:1.]
+
+using Arrow
