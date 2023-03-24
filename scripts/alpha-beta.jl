353ac814ea845f7334f719986dcfe5b909bba7f5
Matteo Smerlak <matteo.smerlak@gmail.com>
2023-03-24 14:30:43 +0100

    alpha-beta plots

--- scripts/alpha-beta.jl
+++ scripts/alpha-beta.jl
@@ -4,31 +4,39 @@ using DrWatson
 using Glob, Revise
 foreach(includet, glob("*.jl", srcdir()))
 
-using Plots, LaTeXStrings
-gr(label = false, grid = false)
+for dist in ("normal", "gamma")
+    function σ_critical(α, β = 1)
+        p = Dict{Symbol, Any}(
+            :scaled => true,
+            :S => 100,
+            :μ => .5,
+            :α => α,
+            :β => β,
+            :γ => 1.,
+            :extinction_threshold => 1e-4,
+            :dist => dist,
+            :N => 2,
+        );
 
-function σ_critical(α, β = 1)
-    p = Dict{Symbol, Any}(
-        :scaled => true,
-        :S => 100,
-        :μ => .5,
-        :α => α,
-        :β => β,
-        :γ => 1.,
-        :extinction_threshold => 1e-4,
-        :dist => "gamma",
-        :N => 4,
-    );
-
-    for σ in 0.0001:.01:5
-        p[:σ] = σ
-        s = stability!(p)
-        s < .9 && return σ
+        for σ in 0.0001:.01:5
+            p[:σ] = σ
+            s = stability!(p)
+            s < .9 && return σ
+        end
     end
+
+    A = .1:.05:2.
+    B = .1:.05:2.
+    Σ = [β > α ? σ_critical(α, β) : 0 for α in A, β in B]
+
+    using DataFrames, Arrow
+    Arrow.write(datadir("alpha-beta-$dist"), DataFrame(Σ, :auto))
+
+    using Plots, LaTeXStrings
+    gr(label = false, grid = false)
+
+    heatmap(A, B, Σ', label = L"\sigma_c")
+    xlabel!(L"\alpha")
+    ylabel!(L"\beta")
+    savefig(plotsdir("alpha-beta-$dist"))
 end
-
-Σ = [β > α ? σ_critical(α, β) : 0 for α in .1:.1:1., β in .1:.1:1.]
-
-Σ = [σ_critical(α, β) for α in .1:.1:1., β in .1:.1:1.]
-
-using Arrow
