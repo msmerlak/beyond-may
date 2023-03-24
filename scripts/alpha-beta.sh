0f3d1429d550394b06bd787d91c67f986123830c
Matteo Smerlak <matteo.smerlak@gmail.com>
2023-03-24 14:53:38 +0100

    bash script

--- scripts/alpha-beta.sh
+++ scripts/alpha-beta.sh
@@ -2,11 +2,11 @@
 
 #SBATCH --job-name=alpha-beta
 #SBATCH --mail-user=matteo.smerlak@gmail.com --mail-type=ALL
-#SBATCH --clusters=mesopsl1
+#SBATCH --clusters=mesopsl2
 #SBATCH --partition=def
-#SBATCH --qos=mesopsl1_def_long
+#SBATCH --qos=mesopsl2_def_long
 #SBATCH --account=smerlak
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=16
+#SBATCH --ntasks-per-node=24
 
-julia --threads=auto /travail/msmerlak/general-stability/scripts/alpha-beta.jl > alpha-beta.out
+julia --threads=auto /travail/msmerlak/general-stability/scripts/alpha-beta.jl > /travail/msmerlak/general-stability/scripts/alpha-beta.out
