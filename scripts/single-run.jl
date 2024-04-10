72fdacbbd947a52d4e57abb1bfdc4013c6a4bd08
Onofrio Mazzarisi <onofrio.mazzarisi@gmail.com>
2024-04-10 17:40:30 +0200

    add self-consistent computation for cut-off

--- scripts/single-run.jl
+++ scripts/single-run.jl
@@ -4,28 +4,23 @@ foreach(include, glob("*.jl", srcdir()))
 
 using Plots, LaTeXStrings
 
-pippo = rand(Uniform(1.1,1.5),100)
-pippo[3] = 1.
-
 p = Dict{Symbol,Any}(
     :scaled => false,
     :S => 100,
-    :μ => 0.75,
-    :μₛ => 1.,
-    :σ => 0.15,
+    :μ => .01,
+    :μₛ => .01,
+    :σ => .01,
     :α => 1.0,
-    :β => pippo,
+    :β => 1.5,
     :γ => 1.0,
-    :x0 => .1*ones(100),
+    :x0 => 1. *ones(100),
     :extinction_threshold => 1e-6,
-    :dist => "normal",
+    :dist => "gamma",
     :N => 1,
-    :seed => rand(UInt)
+    :seed => 7#rand(UInt)
 );
 
 evolve!(p; trajectory=true)
-
-p[:trajectory].u[end]
 
 #= trajectories =#
 plot(
@@ -40,7 +35,7 @@ plot(
 
 #= density distribution =#
 p_eq = p[:trajectory].u[end]
-histogram(p_eq,
+stephist(p_eq,
     normalize=true,
     alpha=0.5,
     color=:green,
@@ -48,6 +43,8 @@ histogram(p_eq,
     xlabel=L"x_*",
     label=false,
     grid=false,
+    fill=true,
+    #bins = minimum(p_eq):.005:maximum(p_eq),
 )
 
 #= cavity solution with gaussian approximation =#
@@ -61,17 +58,19 @@ plot!(X, [pdf(P_gauss(p), x) for x in X],
     grid=false
 )
 
-a, b, c = Cavity(p)
+a, b, c, Λ = Cavity_Λ(p)
 
-X=[x for x in .9*minimum(p_eq):.01:1.1*maximum(p_eq)]
+X=[x for x in .9*minimum(p_eq):.001:1.1*maximum(p_eq)]
 plot!(X,[P_n(x, b, c, p) for x in X],
 labels = false, #"cavity - gaussian",
 linewidth = 2,
 alpha = 1,
 linecolor = :black,
 grid = false,
-size = (600,200)
+size = (600,200),
 )
+
+(100*exp(-p[:μ]^2*100^2*1.04^2/(2*p[:σ]^2*100*1.42))/(sqrt(2*pi*p[:σ]^2*100*1.42)))^(2)
 
 p[:converged]
 
--- src/cavity.jl
+++ src/cavity.jl
@@ -4,31 +4,59 @@ using DifferentialEquations
 using LinearAlgebra
 using QuadGK
 using HCubature
-using NLsolve
+using NLsolve, Roots
 using SpecialFunctions
-
-#= expression for density distribution with gaussian approximation (γ=1) =#
-function P_gauss(p)
-    @unpack μ, σ, α, β, S = p
-    μ = p[:scaled] ? μ : μ*S
-    σ = p[:scaled] ? σ : σ*sqrt(S) 
-    e1 = μ^(1/(α-β-1))
-    e2 = e1^2/(1-(1/(α-β))^2*(μ*e1)^(2*(1-(α-β))/(α-β))*σ^2)
-    #ϕ = e2>0 ? (1+erf((e1)/sqrt(2*(e2-e1^2))))/2 : 1
-    return e2>0 ? Normal(e1, sqrt(e2-e1^2)) : Normal(e1, 0) #(e1, e2>0 ? e2 : 0, ϕ, e2>0 ? Normal(e1,sqrt(e2-e1^2)) : Normal(e1,0))
-end
 
 #= expression for density distribution (γ=1) =#
 function P_n(n, e1_n, e2_n, p)
-    @unpack μ, σ, α, β = p
+    @unpack μ, σ, α, β, S = p
     μ = p[:scaled] ? μ : μ*S
     σ = p[:scaled] ? σ : σ*sqrt(S)
-    return abs(α-β)*n^(α-β-1)/(sqrt(2*π*σ^2*e2_n))*
-        exp(-(n^(α-β)-μ*e1_n)^2/(2*σ^2*e2_n))
+    x = max(n,1e-20)
+    return abs(α-β)*x^(α-β-1)/(sqrt(2*π*σ^2*e2_n))*exp(-(x^(α-β)-μ*e1_n)^2/(2*σ^2*e2_n))
 end
 
 #= cavity calculation for fraction of positive equilibriums 
 and first two moments of the equilibrium distribution given μ and σ. =#
+function Cavity_Λ(p;
+    Iter = 2000, #number of iteration
+    rela = .01, #relax parameter for fixed point
+    tol = 1e-9, #requested tolerance for numerical integrator
+    n_min = p[:extinction_threshold], #lower bound for integration
+    Λ_init = 1, #initial guess for cut-off
+    e1_init = (p[:scaled] ? 1/p[:μ] : 1/p[:μ]/p[:S]), #initial guess for e1_n
+    e2_init = (p[:scaled] ? 1/p[:μ] : 1/p[:μ]/p[:S]), #initial guess for e1_n
+    )
+
+    ϕ_n = ones(Iter+1) #fraction of survivals
+    Λ_n = Λ_init*ones(Iter+1) #cut-off
+    e1_n = e1_init*ones(Iter+1) #<n>
+    e2_n = e2_init*ones(Iter+1) #<n²>
+
+    for i in 1:Iter
+    #    ϕ_new = first(quadgk(x -> P_n(x, e1_n[i], e2_n[i], p) , n_min, Λ_n[i], rtol=tol))
+        e1_new = first(quadgk(x -> x*P_n(x, e1_n[i], e2_n[i], p) , n_min, Λ_n[i], rtol=tol))
+        e2_new = first(quadgk(x -> x*x*P_n(x, e1_n[i], e2_n[i], p) , n_min, Λ_n[i], rtol=tol))
+
+        g(λ) = 1 - first(quadgk(x -> P_n(x, e1_n[i], e2_n[i], p) , 0, λ, rtol=tol)) - 1/p[:S]
+
+        Λ_new = find_zero(g, Λ_n[i])
+
+    
+    #    ϕ_n[i+1] = (1-rela)*ϕ_n[i] + rela*ϕ_new
+        Λ_n[i+1] = (1-rela)*Λ_n[i] + rela*Λ_new        
+        e1_n[i+1] = (1-rela)*e1_n[i] + rela*e1_new
+        e2_n[i+1] = (1-rela)*e2_n[i] + rela*e2_new
+    end
+    ϕ_n_eq, e1_n_eq, e2_n_eq, Λ_n_eq = ϕ_n[Iter], e1_n[Iter], e2_n[Iter], Λ_n[Iter]
+    return (ϕ_n_eq, e1_n_eq, e2_n_eq, Λ_n_eq)
+end
+
+#= cavity calculation for fraction of positive equilibriums 
+and first two moments of the equilibrium distribution given μ and σ.
+In this case we keep Λ=n_max constant to speed up computations and vaoid to 
+find the right ansatz for initial condition case by case.
+Results are not affected by this approximation.=#
 function Cavity(p;
     Iter = 2000, #number of iteration
     rela = .01, #relax parameter for fixed point
@@ -91,6 +119,48 @@ function σ_crit(p;
     return (σc_eq, ϕ_n_eq, e1_n_eq, e2_n_eq)
 end
 
+#= critical line in μ - σ space =#
+function critical_line(p;
+    μ_range = (.005:.005:.1),
+    n_max = 1e3,
+    )
+    σc_line = ones(length(μ_range)) 
+    for (i,μ) in enumerate(μ_range)
+        p[:μ] = μ
+        σc_line[i] = first(σ_crit(p; n_max))
+        @show σc_line
+    end
+    return μ_range, σc_line
+end
+
+
+#= Gaussian approximation for the distribution and critical line for this case. =#
+
+#= expression for density distribution with gaussian approximation (γ=1) =#
+function P_gauss(p)
+    @unpack μ, σ, α, β, S = p
+    μ = p[:scaled] ? μ : μ*S
+    σ = p[:scaled] ? σ : σ*sqrt(S) 
+    e1 = μ^(1/(α-β-1))
+    e2 = e1^2/(1-(1/(α-β))^2*(μ*e1)^(2*(1-(α-β))/(α-β))*σ^2)
+    #ϕ = e2>0 ? (1+erf((e1)/sqrt(2*(e2-e1^2))))/2 : 1
+    return e2>0 ? Normal(e1, sqrt(e2-e1^2)) : Normal(e1, 0) #(e1, e2>0 ? e2 : 0, ϕ, e2>0 ? Normal(e1,sqrt(e2-e1^2)) : Normal(e1,0))
+end
+
+
+#= critical line in μ - σ space for gaussian approximation =#
+function critical_line_gauss(p;
+    μ_range = (.005:.005:.1),
+    )
+    σc_line = ones(length(μ_range)) 
+    for (i,μ) in enumerate(μ_range)
+        p[:μ] = μ
+        σc_line[i] = σ_crit_gauss(p)
+        @show σc_line
+    end
+    return μ_range, σc_line
+end
+
 #= critical σ, variant with gaussian approximation =#
 function σ_crit_gauss(p;
     Iter = 2000, #number of iteration
@@ -110,31 +180,4 @@ function σ_crit_gauss(p;
     end
     σc_eq = σc[Iter]
     return σc_eq
-end
-
-#= critical line in μ - σ space =#
-function critical_line(p;
-    μ_range = (.005:.005:.1),
-    n_max = 1e3,
-    )
-    σc_line = ones(length(μ_range)) 
-    for (i,μ) in enumerate(μ_range)
-        p[:μ] = μ
-        σc_line[i] = first(σ_crit(p; n_max))
-        @show σc_line
-    end
-    return μ_range, σc_line
-end
-
-#= critical line in μ - σ space for gaussian approximation =#
-function critical_line_gauss(p;
-    μ_range = (.005:.005:.1),
-    )
-    σc_line = ones(length(μ_range)) 
-    for (i,μ) in enumerate(μ_range)
-        p[:μ] = μ
-        σc_line[i] = σ_crit_gauss(p)
-        @show σc_line
-    end
-    return μ_range, σc_line
 end
