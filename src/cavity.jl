b775ce8354f651ce0c50e14446862bf913627990
Onofrio Mazzarisi <onofrio.mazzarisi@gmail.com>
2024-04-16 13:16:39 +0200

    update cavity.jl with cut-off

--- src/cavity.jl
+++ src/cavity.jl
@@ -54,14 +54,15 @@ end
 
 #= cavity calculation for fraction of positive equilibriums 
 and first two moments of the equilibrium distribution given μ and σ.
-In this case we keep Λ=n_max constant to speed up computations and vaoid to 
-find the right ansatz for initial condition case by case.
+In this case we keep Λ = S^(β-α) constant to speed up computations and avoid to find the right ansatz for initial condition case by case.
+The choice is an analitical approximation of the scaling of Λ with S.
+It can also be chosen to be a fixed constant 10^2 or 10^3.
 Results are not affected by this approximation.=#
 function Cavity(p;
     Iter = 2000, #number of iteration
     rela = .01, #relax parameter for fixed point
     tol = 1e-9, #requested tolerance for numerical integrator
-    n_max = 1e4, #upper bound for integration
+    Λ = p[:S]^(p[:β]-p[:α]), #Cut-off for integration
     n_min = p[:extinction_threshold], #lower bound for integration
     e1_init = (p[:scaled] ? 1/p[:μ] : 1/p[:μ]/p[:S]), #initial guess for e1_n
     e2_init = (p[:scaled] ? 1/p[:μ] : 1/p[:μ]/p[:S]) #initial guess for e1_n
@@ -72,9 +73,9 @@ function Cavity(p;
     e2_n = e2_init*ones(Iter+1) #<n²>
 
     for i in 1:Iter
-    #    ϕ_new = first(quadgk(x -> P_n(x, e1_n[i], e2_n[i], p) , n_min, n_max, rtol=tol))
-        e1_new = first(quadgk(x -> x*P_n(x, e1_n[i], e2_n[i], p) , n_min, n_max, rtol=tol))
-        e2_new = first(quadgk(x -> x*x*P_n(x, e1_n[i], e2_n[i], p) , n_min, n_max, rtol=tol))
+    #    ϕ_new = first(quadgk(x -> P_n(x, e1_n[i], e2_n[i], p) , n_min, Λ, rtol=tol))
+        e1_new = first(quadgk(x -> x*P_n(x, e1_n[i], e2_n[i], p) , n_min, Λ, rtol=tol))
+        e2_new = first(quadgk(x -> x*x*P_n(x, e1_n[i], e2_n[i], p) , n_min, Λ, rtol=tol))
 
     #    ϕ_n[i+1] = (1-rela)*ϕ_n[i] + rela*ϕ_new
         e1_n[i+1] = (1-rela)*e1_n[i] + rela*e1_new
@@ -92,7 +93,7 @@ function σ_crit(p;
     e1_init = (p[:scaled] ? 1/p[:μ] : 1/p[:μ]/p[:S]), #initial guess for e1_n
     e2_init = (p[:scaled] ? 1/p[:μ] : 1/p[:μ]/p[:S]), #initial guess for e1_n
     σc_init = (p[:scaled] ? .1*p[:μ] : .1*p[:μ]*p[:S]), #initial guess for σ_c
-    n_max = 1e3, #upper bound for integration
+    Λ = 1e3, #upper bound for integration
     n_min = p[:extinction_threshold], #lower bound for integration
     )
     @unpack μ, σ, α, β = p
@@ -102,11 +103,11 @@ function σ_crit(p;
     σc = σc_init*ones(Iter+1) 
     for i in 1:Iter
         p[:σ] = σc[i]
-#        ϕ_new = first(quadgk(x -> P_n(x, e1_n[i], e2_n[i], p) , n_min, n_max, rtol=tol))
-        e1_new = first(quadgk(x -> x*P_n(x, e1_n[i], e2_n[i], p) , n_min, n_max, rtol=tol))
-        e2_new = first(quadgk(x -> x*x*P_n(x, e1_n[i], e2_n[i], p) , n_min, n_max, rtol=tol))
+#        ϕ_new = first(quadgk(x -> P_n(x, e1_n[i], e2_n[i], p) , n_min, Λ, rtol=tol))
+        e1_new = first(quadgk(x -> x*P_n(x, e1_n[i], e2_n[i], p) , n_min, Λ, rtol=tol))
+        e2_new = first(quadgk(x -> x*x*P_n(x, e1_n[i], e2_n[i], p) , n_min, Λ, rtol=tol))
         σc_new = abs(α-β)^(1/2)*((first(quadgk(x -> P_n(x, e1_n[i], e2_n[i], p)*x^(2*(β-α+1)),
-        n_min, n_max, rtol=tol))))^(-1/2)
+        n_min, Λ, rtol=tol))))^(-1/2)
 
         
         #ϕ_n[i+1] = (1-rela)*ϕ_n[i] + rela*ϕ_new
@@ -122,12 +123,12 @@ end
 #= critical line in μ - σ space =#
 function critical_line(p;
     μ_range = (.005:.005:.1),
-    n_max = 1e3,
+    Λ = 1e3,
     )
     σc_line = ones(length(μ_range)) 
     for (i,μ) in enumerate(μ_range)
         p[:μ] = μ
-        σc_line[i] = first(σ_crit(p; n_max))
+        σc_line[i] = first(σ_crit(p; Λ))
         @show σc_line
     end
     return μ_range, σc_line
@@ -167,7 +168,7 @@ function σ_crit_gauss(p;
     rela = .01, #relax parameter for fixed point
     tol = 1e-9, #requested tolerance for numerical integrator
     σc_init = (p[:scaled] ? .1*p[:μ] : .1*p[:μ]*p[:S]), #initial guess for σ_c
-    n_max = 1e2, #upper bound for integration
+    Λ = 1e2, #upper bound for integration
     n_min = 0, #lower bound for integration
     )
     @unpack μ, σ, α, β = p
@@ -175,7 +176,7 @@ function σ_crit_gauss(p;
     for i in 1:Iter
         p[:σ] = σc[i]
         σc_new = abs(α-β)*((first(quadgk(x -> pdf(P_gauss(p), x)*x^(2*(β-α+1)) ,
-            n_min, n_max, rtol=tol))))^(-1/2)
+            n_min, Λ, rtol=tol))))^(-1/2)
         σc[i+1] = (1-rela)*σc[i] + rela*σc_new
     end
     σc_eq = σc[Iter]
